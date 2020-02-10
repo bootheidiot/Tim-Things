a648a48432026b336db94d4771588f0f190040e5
bootheidiot <50155485+bootheidiot@users.noreply.github.com>
2020-02-10 10:05:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-<object width="550" height="400"></object>
-<embed src="Swords and Sandals 2 (Full).swf" width="720" height="480"></embed>
+var select = document.getElementById("select");
+
+function checkselect() {
+  alert(select.value);
+}
